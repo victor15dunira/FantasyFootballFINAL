b40df6782999e2d12b990279d2e8732b0b8fc33f
victor15dunira <victor.dunira@avanade.com>
2021-05-05 16:25:47 +0100

    Add project files.

---
+++ FantasyFootballWebApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FantasyFootballWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FantasyFootballWebApp.Controllers
+{
+    public class PlayersController : Controller
+    {
+        private readonly ILogger<PlayersController> _logger;
+
+        public PlayersController(ILogger<PlayersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FantasyFootballWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using FantasyFootballWebLibrary.Models;
+using FantasyFootballWebLibrary.Models.Binding;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FantasyFootballWebLibrary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        //MAPPING OUT THE DATABASE
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        //public DbSet<Vet> Vets { get; set; }
+        //public DbSet<Visit> Visits { get; set; }
+
+
+
+    }
+}
+
