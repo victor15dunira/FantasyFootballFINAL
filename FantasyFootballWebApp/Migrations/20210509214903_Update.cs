ed0447b58750746764fffb065d8466b6877ecc57
victor15dunira <victor.dunira@avanade.com>
2021-05-09 23:11:10 +0100

    Final Update

--- FantasyFootballWebApp/Migrations/20210509214903_Update.cs
+++ FantasyFootballWebApp/Migrations/20210509214903_Update.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace FantasyFootballWebApp.Migrations
 {
-    public partial class FirstTest : Migration
+    public partial class Update : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
---
+++ TeamsandPlayers.Tests/PlayerController.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TeamsandPlayers.Tests
+{
+    public class PlayerControllerTest
+    {
+        [Fact]
+        public void GetAllPlayers_Test ()
+        {
+
+        }
+    }
+}
